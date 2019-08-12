9a20344d5101d2df8f9af10cabc3e08e8e52bc79
snvankit07 <17143910+snvankit07@users.noreply.github.com>
2019-08-12 18:55:43 +0530

    update

--- assets/js/GMB.js
+++ assets/js/GMB.js
@@ -1,9 +1,32 @@
-$(document).on('click', '.table-box-select-new table tr', function () {
-    $(this).find('td label').toggleClass('active');
+$(document).on('click', '.table-box-select-new table tr td', function() {
+    $(this).find('label').toggleClass('active');
     //$('.table-box-select-new table tbody tr td label input').show();
-    $('.table-box-select-new tr td label').each(function () {
-        if ($(this).hasClass('active'))
-        {
+    $('.table-box-select-new tr td label').each(function() {
+        if ($(this).hasClass('active')) {
+            $(this).find('input').prop("checked", true);
+        } else {
+            $(this).find('input').prop("checked", false);
+        }
+    });
+});
+
+$(document).on('click', '.table-box-select-new table tr td label', function() {
+    $(this).toggleClass('active');
+    //$('.table-box-select-new table tbody tr td label input').show();
+    $('.table-box-select-new tr td label').each(function() {
+        if ($(this).hasClass('active')) {
+            $(this).find('input').prop("checked", true);
+        } else {
+            $(this).find('input').prop("checked", false);
+        }
+    });
+});
+
+$(document).on('click', '.table-box-select-new table tr td span', function() {
+    $(this).parent('label').toggleClass('active');
+    //$('.table-box-select-new table tbody tr td label input').show();
+    $('.table-box-select-new tr td label').each(function() {
+        if ($(this).hasClass('active')) {
             $(this).find('input').prop("checked", true);
         } else {
             $(this).find('input').prop("checked", false);
