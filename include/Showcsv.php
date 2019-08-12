f0c66e5a042ee0163ed1ccef91e46bcf062a5d36
snvankit07 <17143910+snvankit07@users.noreply.github.com>
2019-08-12 19:30:27 +0530

    update

--- include/Showcsv.php
+++ include/Showcsv.php
@@ -1,33 +1,37 @@
 <?php
 if (isset($_SESSION["ShowCSV"]) && $_SESSION["ShowCSV"] == 1) {
-    $target_dir = "assets/uploads/" . session_id() . '/';
-    $target_file = $target_dir . (session_id() . '.csv');
-    $file = fopen($target_file, "r");
-    echo '<div class="overflow"><table class="table">';
-    $i=0;
-    while (!feof($file)) {
-        echo '<tr>';
-        $col='td';
-        if($i==0)
-        {
-            $col='th';
-        }
-        $FileData=(fgetcsv($file));
-        $j=0;
-        foreach ($FileData as $v)
-        {
-            echo '<'.$col.'>'.$v.'</'.$col.'>';
-        $j++;
+      $target_dir = "assets/uploads/" . session_id() . '/';
+      $target_file = $target_dir . (session_id() . '.csv');
+      $file = fopen($target_file, "r");
+      echo '<div class="overflow"><table class="table">';
+      $i = 0;
+      while (!feof($file)) {
+            echo '<tr>';
+            $col = 'td';
+            if ($i == 0) {
+                  $col = 'th';
+            }
+            $FileData = (fgetcsv($file));
+            $j = 0;
+            foreach ($FileData as $v) {
+                  echo '<' . $col . '>' . $v . '</' . $col . '>';
+                  $j++;
+            }
+            echo '</tr>';
 
-        }
-        echo '</tr>';
-
-        $i++;
-
-    }
-    echo '</table></div>';
-    fclose($file);
+            $i++;
+      }
+      echo '</table></div>';
+      fclose($file);
 }
 ?><br><br>
 <div class="results"><div class="col-xs-12"></div></div>
 <center><a class="btn btn-primary">Now synchronize <i class="fa fa-refresh"></i></a></center>
+
+<style>
+      body .alert-success {
+            color: #ffffff !important;
+            background-color: #4a7538 !important;
+            border-color: #b6bbb1 !important;
+      }
+</style>
